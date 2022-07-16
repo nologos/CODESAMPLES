83cdbf2b81153eaa57c0221afa2f6baeb617833e
nologos <git.nologos@outlook.com>
2022-07-16 22:38:33 +0100

    multi

---
+++ GIT/Github/workingWithBranches.md
@@ -0,0 +1,6 @@
+# get curent branch sha
+
+the branches are not the same on the local computer vs remote server
+
+git rev-parse origin/master # to get the latest commit on the remote
+git rev-parse HEAD          # to get the latest commit on the local 
---
+++ POWERSHELL/firewall.ps1
@@ -0,0 +1,52 @@
+$paramsplat = @{
+    displayname = "Allow SSH on local physical connection"
+    description = "Allow SSH on local physical connection"
+    localaddress = "10.0.0.2/8"
+    localport = 22
+    remoteaddress = "10.0.0.1/8"
+    protocol = "tcp"
+    action = "allow"
+    direction = "Inbound"
+    enabled = "false"
+}
+new-netfirewallrule @paramsplat
+
+# https://docs.microsoft.com/en-us/windows-server/administration/openssh/openssh_install_firstuse
+Get-WindowsCapability -Online | Where-Object Name -like 'OpenSSH*'
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+
+# Start the sshd service
+Start-Service sshd
+
+# OPTIONAL but recommended:
+Set-Service -Name sshd -StartupType 'Automatic'
+
+# Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
+if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
+    Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
+    New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+} else {
+    Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
+}
+
+
+
+
+$splat = @{
+Name = "OpenSSH-Server-In-TCP"
+DisplayName = "OpenSSH Server (sshd)"
+Enabled = "True"
+Direction = "Inbound"
+Protocol = "TCP"
+Action = "Allow"
+LocalPort = "22"
+}
+
+New-NetFirewallRule @splat
+
+
+
+
+## configuring local networks from public to domain
+
+get-netaddaper
