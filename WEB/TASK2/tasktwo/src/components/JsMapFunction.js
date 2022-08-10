1dcf3b5e98db6a542ca6ffeea8298c5a29806ac9
nologos <git.nologos@outlook.com>
2022-08-10 23:46:50 +0100

    adding tasks, forms completed

---
+++ WEB/TASK2/tasktwo/src/components/JsMapFunction.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+// foreach number add 5 and console log it
+const JsMapFunction = () => {
+  const newNumbers = numbers.map((number) => {
+    return number + 5;
+  });
+  return (
+    <div className="container my-5">
+      <div className="row bg-primary g-3 pb-4">
+      <h1>JsMapFunction</h1>
+        {newNumbers.map((number) => {
+          return (
+            <div key={number} className="col-1">
+                <div className="p-1 bg-light border border-radius-1 border-color-dark d-flex justify-content-center align-items-center mb-0">
+                  {number}
+                </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default JsMapFunction;
--- WEB/TASK2/tasktwo/src/main.js
+++ WEB/TASK2/tasktwo/src/main.js
@@ -4,23 +4,30 @@ import Tasks from "./components/Tasks";
 import { useState } from "react";
 import NoTask from "./components/NoTask";
 import AddTask from "./components/AddTask";
+import { FaTwitch } from "react-icons/fa";
 
 
-const onClick = () => {
+
+
+const onClick = (tasks) => {
   console.log("clicked");
+  console.log(tasks)
 };
 
 const main = () => {
+  const startDate = ""
   const [tasks, setTasks] = useState([
-    { id: 1, title: "Task 1", text: "Lorem ipsum dolor", completed: false },
-    { id: 2, title: "Task 2", text: "ipsum dolor sit", completed: false },
-    { id: 3, title: "Task 3", text: "Lorem  dolor sit", completed: false },
-    { id: 4, title: "Task 4", text: "Lorem ipsum  sit", completed: false },
+    { id: 1, title: "Task 1", text: "Lorem ipsum dolor",date:startDate, completed: false },
+    { id: 2, title: "Task 2", text: "ipsum dolor sit", date:startDate, completed: false },
+    { id: 3, title: "Task 3", text: "Lorem  dolor sit", date:startDate, completed: false },
+    { id: 4, title: "Task 4", text: "Lorem ipsum  sit", date:startDate, completed: false },
   ]);
-
+  console.log(tasks);
   const AddNewTask = (task) => {
-    console.out(task)
-  }
+    const id = Math.floor(Math.random() * 1000)+1;
+    const newTask = {  id, ...task };
+    setTasks([...tasks, newTask])
+  };
 
   const deleteItem = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
@@ -32,21 +39,21 @@ const main = () => {
 
   return (
     <div style={{ backgroundColor: "beige" }}>
+      <div className="d-flex justify-content-center m-4 p-4">
+        <FaTwitch size={300} />
+      </div>
+
       <div className="container d-flex justify-content-around p-1">
         <Onebutton color="yellow" text="press me easy" onClick={onClick} />
-        <Onebutton color="yellow" text="press me easy" onClick={onClick} />
+        <div type="submit" className="btn btn-primary" onClick={onClick}>function btn</div>
+        <a href="#122" className="btn border-dark">test</a>
       </div>
       <div className="container">{tasks.length > 0 ? <Tasks tasks={tasks} deleteItem={deleteItem} completeItem={completeItem}></Tasks> : <NoTask></NoTask>}</div>
       <div className="d-flex justify-content-between">
-        <AddTask 
-        onAdd={AddNewTask}
-
-            
-        ></AddTask>
+        <AddTask onAdd={AddNewTask}></AddTask>
       </div>
     </div>
   );
 };
-
 
 export default main;
