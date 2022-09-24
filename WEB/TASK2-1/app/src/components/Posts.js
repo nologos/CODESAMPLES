ba27d931cdc34066fe666eb3e5f7e953c1e469a0
nologos <git.nologos@outlook.com>
2022-09-24 21:03:13 +0100

    app rename to header, including posts

--- WEB/TASK2-1/app/src/components/Posts.js
+++ WEB/TASK2-1/app/src/components/Posts.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
 import Post from "./Post";
 
 const Posts = () => {
   return (
-    <div className="container"><Post /></div>
-    
-  )
-}
+    <div className="container">
+      <Post />
+    </div>
+  );
+};
 
-export default Posts
+export default Posts;
--- WEB/TASK2-1/app/src/index.js
+++ WEB/TASK2-1/app/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Header from './components/Header';
 import reportWebVitals from './reportWebVitals';
+import Posts from './components/Posts';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <Posts />
   </React.StrictMode>
 );
 
